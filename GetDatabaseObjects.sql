0692e27485d0ad2dabf89b5818db48cc24b8da46
apisitphum <96420127+apisitphum@users.noreply.github.com>
2022-01-07 10:54:11 +0700

    Update GetDatabaseObjects.sql
    add 
    - find text in store procedure
    - columns schemas

--- GetDatabaseObjects.sql
+++ GetDatabaseObjects.sql
@@ -3,3 +3,18 @@ Select * From sys.objects ;
 
 -- link servers
 Exec sp_linkedservers ;
+
+-- find text in store prcedure
+Declare @FindText Varchar(1000) = 'FindText'
+
+Select [Scehma]=schema_name(o.schema_id), o.Name, o.type 
+  From sys.sql_modules m
+  Inner Join sys.objects o
+  On o.object_id = m.object_id
+  Where m.definition Like '%' + @FindText + '%'
+  
+ -- table columns schemas
+Declare @TableName Varchar(1000) = 'TableName'
+
+Select * From INFORMATION_SCHEMA.COLUMNS 
+    Where [TABLE_NAME] Like '%' + @TableName + '%'
