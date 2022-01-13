3fb685bc64a36ecfbd7ae9ec3880653324518caf
apisitphum <96420127+apisitphum@users.noreply.github.com>
2022-01-13 16:17:26 +0700

    Create Enable xp_cmdshell.sql
    Enable xp_cmdshell

---
+++ Enable xp_cmdshell.sql
@@ -0,0 +1,10 @@
+/*
+
+   Error : SQL Server blocked access to procedure 'sys.xp_cmdshell' of component 'xp_cmdshell' because this component is turned off as part of the security configuration for this server.
+   Step 
+    1) Right click on server properties
+    2) Select [Faclt]
+    3) Select [Server security]
+    4) Set [XpCmdShellEnabled] = true
+
+*/
