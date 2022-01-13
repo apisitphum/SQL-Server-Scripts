1eaa6d3359d8d30cb82be3c59e654aa2e607c19a
apisitphum <96420127+apisitphum@users.noreply.github.com>
2022-01-13 13:43:58 +0700

    Create DatabaseMail.sql
    enabled database mail feature

---
+++ DatabaseMail.sql
@@ -0,0 +1,16 @@
+/*
+Database Mail XPs Server Configuration
+   0: Database Mail isn't available (default)
+   1: Database Mail is available
+*/
+
+IF EXISTS (
+    SELECT 1 FROM sys.configurations 
+    WHERE NAME = 'Database Mail XPs' AND VALUE = 0)
+BEGIN
+  PRINT 'Enabling Database Mail XPs'
+  EXEC sp_configure 'show advanced options', 1;  
+  RECONFIGURE
+  EXEC sp_configure 'Database Mail XPs', 1;  
+  RECONFIGURE  
+END
